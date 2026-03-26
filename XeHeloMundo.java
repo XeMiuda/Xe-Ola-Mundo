e4d6c2ea93a5659c7d095ddd8bc28aacc095301a
XeMiuda <xemiuda@gmail.com>
2026-03-26 11:38:27 +0000

    Update XeHeloMundo.java
    Segunda edição de Ola, Mundo!

--- XeHeloMundo.java
+++ XeHeloMundo.java
@@ -2,6 +2,6 @@ import xe.olamundo
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Ola, Mundo!");
     }
 }
